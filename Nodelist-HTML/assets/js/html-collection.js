3ffbdb50f92e5278e76fc431a6fa6eb649b942bb
pydovah37 <xterm.conf@gmail.com>
2022-06-30 13:11:17 -0700

    Adding new projects

---
+++ Nodelist-HTML/assets/js/html-collection.js
@@ -0,0 +1,10 @@
+export default () => {
+  const bodyChildElements = document.body.children; // HTMLCollection [div]
+
+  const div = bodyChildElements[0];
+
+  const divChildElements = div.children; // HTMLCollection [span]
+  const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
+  const span = divChildElements[0]; // <span>Yes!</span>
+  // debugger;
+};
