18ba027fd09b670b8568f33a7f9e127f232459b6
pydovah37 <xterm.conf@gmail.com>
2022-07-01 13:52:52 -0700

    element selection project, halfway through

--- Element-Selection/selectors.js
+++ Element-Selection/selectors.js
@@ -1,38 +1,44 @@
-const select = () => {
-    /* Write queries for each of the following */
+window.onload = function () {
+  /* Write queries for each of the following */
 
-    /* Section 1 */
-    // 1. Get all seeded fruit elements
-    // Your code here
-    
-    // 2. Get all seedless fruit elements
-    // Your code here
+  /* Section 1 */
+  // 1. Get all seeded fruit elements
+  const bodyChildElements = document.body.children;
+  console.log(bodyChildElements);
+  const seedFruit = bodyChildElements[1].getElementsByClassName("seed");
 
-    // 3. Get first seedless fruit element
-    // Your code here
+  // 2. Get all seedless fruit elements
+  const seedlessFruit = bodyChildElements[1].getElementsByClassName("seedless");
 
-    /* Section 2 */
-    // 4. Get inner span with text "you"
-    // Your code here
+  // 3. Get first seedless fruit element
+  const peaches = seedlessFruit[0];
+  console.log(peaches);
+  peaches.style.color = "red";
 
-    // 5. Get all children of element "wrapper"
-    // Your code here
+  /* Section 2 */
+  // 4. Get inner span with text "you"
+  const bodyNodeElements = document.body.childNodes;
+  console.log(bodyNodeElements);
+  const youText = bodyNodeElements[5].textContent;
+  console.log(youText)
 
-    // 6. Get all odd number list items in the list
-    // Your code here
 
-    // 7. Get all even number list items in the list
-    // Your code here
+  // 5. Get all children of element "wrapper"
+  // Your code here
 
-    /* Section 3 */
-    // 8. Get all tech companies without a class name
-    // Your code here
+  // 6. Get all odd number list items in the list
+  // Your code here
 
-    // 9. Get "Amazon" list element
-    // Your code here
+  // 7. Get all even number list items in the list
+  // Your code here
 
-    // 10. Get all unicorn list elements (not the image element)
-    // Your code here
-}
+  /* Section 3 */
+  // 8. Get all tech companies without a class name
+  // Your code here
 
-window.onload = select;
+  // 9. Get "Amazon" list element
+  // Your code here
+
+  // 10. Get all unicorn list elements (not the image element)
+  // Your code here
+};
